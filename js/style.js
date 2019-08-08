d213e5862bb14b519da553e47cc30b60f6355ecd
natephil <41304546+natephil@users.noreply.github.com>
2019-08-08 08:25:06 -0700

    Update style.js

--- js/style.js
+++ js/style.js
@@ -7,12 +7,12 @@ let observer = new IntersectionObserver(entries => {
   if (entries[0].boundingClientRect.y < 0) {
     document.body.classList.add("header-not-at-top");
   } else {
-    document.body.classList.remove("header-not-at-top");
+    document.body.classList.remove("fixed");
   }
 });
-observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
+observer.observe(document.querySelector("fixed"));
 }
 
-('#top').click(function() { // when the button is clicked
+('fixed').click(function() { // when the button is clicked
     $('body,html').animate({scrollTop:0},500); // return to the top with a nice animation
 });
