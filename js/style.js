f32ff0b9d5eed3af862aad3538ee815ab1cab2d4
natephil <41304546+natephil@users.noreply.github.com>
2019-08-08 06:48:01 -0700

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,11 +1,14 @@
-(window).scroll(function() { // when the page is scrolled run this
-    if($(this).scrollTop() != 0) { // if you're NOT at the top
-        $('.fixed').fadeIn("fast"); // fade in
-    } else { // else
-        $('.fixed').fadeOut("fast"); // fade out
-    }
+if (
+  "IntersectionObserver" in window &&
+  "IntersectionObserverEntry" in window &&
+  "intersectionRatio" in window.IntersectionObserverEntry.prototype
+) {
+let observer = new IntersectionObserver(entries => {
+  if (entries[0].boundingClientRect.y < 0) {
+    document.body.classList.add("header-not-at-top");
+  } else {
+    document.body.classList.remove("header-not-at-top");
+  }
 });
-
-('.fixed').click(function() { // when the button is clicked
-    $('body,html').animate({scrollTop:0},500); // return to the top with a nice animation
-});
+observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
+}
