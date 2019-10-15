5690c91a92222289c6e903c524dbe71a53dedd1d
natephil <41304546+natephil@users.noreply.github.com>
2019-10-15 12:38:18 -0700

    Update sections.js

--- js/sections.js
+++ js/sections.js
@@ -1,40 +1,26 @@
-
-/**
- * scrollVis - encapsulates
+/*scrollVis - encapsulates
  * all the code for the visualization
  * using reusable charts pattern:
  * http://bost.ocks.org/mike/chart/
  */
 var scrollVis = function () {
-  // constants to define the size
-  // and margins of the vis area.
+  // constants to define the size and margins of the vis area.
   var width = 700;
   var height = 520;
   var margin = { top: 0, left: 20, bottom: 40, right: 10 };
 
-  // Keep track of which visualization
-  // we are on and which was the last
-  // index activated. When user scrolls
-  // quickly, we want to call all the
-  // activate functions that they pass.
+  // Keep track of which visualization we are on and which was the last index activated. 
+  // When user scrolls quickly, we want to call all the activate functions that they pass.
   var lastIndex = -1;
   var activeIndex = 0;
-
-  // Sizing for the grid visualization
-  var squareSize = 6;
-  var squarePad = 2;
-  var numPerRow = width / (squareSize + squarePad);
 
   // main svg used for visualization
   var svg = null;
 
-  // d3 selection that will be used
-  // for displaying visualizations
+  // d3 selection that will be used for displaying visualizations
   var g = null;
 
-  // We will set the domain when the
-  // data is processed.
-  // @v4 using new scale names
+  // We will set the domain when the data is processed.
   var xBarScale = d3.scaleLinear()
     .range([0, width]);
 
@@ -180,7 +166,7 @@ var scrollVis = function () {
     g.selectAll('.openvis-title')
       .attr('opacity', 0);
 
-	// intro text title #2
+  // intro text title #2
     // count filler word count title
     g.append('text')
       .attr('class', 'title count-title highlight')
@@ -197,51 +183,6 @@ var scrollVis = function () {
     g.selectAll('.count-title')
       .attr('opacity', 0);
 
-    // square grid
-    // @v4 Using .merge here to ensure
-    // new and old data have same attrs applied
-    // this is the grid of sqaures
-
-    var squares = g.selectAll('.square').data(wordData, function (d) { return d.word; });
-    var squaresE = squares.enter()
-      .append('rect')
-      .classed('square', true);
-    squares = squares.merge(squaresE)
-      .attr('width', squareSize)
-      .attr('height', squareSize)
-      .attr('fill', '#fff')
-      .classed('fill-square', function (d) { return d.filler; })
-      .attr('x', function (d) { return d.x;})
-      .attr('y', function (d) { return d.y;})
-      .attr('opacity', 0);
-
-    // barchart
-    // @v4 Using .merge here to ensure
-    // new and old data have same attrs applied
-    var bars = g.selectAll('.bar').data(fillerCounts);
-    var barsE = bars.enter()
-      .append('rect')
-      .attr('class', 'bar');
-    bars = bars.merge(barsE)
-      .attr('x', 0)
-      .attr('y', function (d, i) { return yBarScale(i);})
-      .attr('fill', function (d, i) { return barColors[i]; })
-      .attr('width', 0)
-      .attr('height', yBarScale.bandwidth());
-
-    var barText = g.selectAll('.bar-text').data(fillerCounts);
-    barText.enter()
-      .append('text')
-      .attr('class', 'bar-text')
-      .text(function (d) { return d.key + '…'; })
-      .attr('x', 0)
-      .attr('dx', 15)
-      .attr('y', function (d, i) { return yBarScale(i);})
-      .attr('dy', yBarScale.bandwidth() / 1.2)
-      .style('font-size', '110px')
-      .attr('fill', 'white')
-      .attr('opacity', 0);
-
     // histogram
     // @v4 Using .merge here to ensure
     // new and old data have same attrs applied
@@ -254,92 +195,37 @@ var scrollVis = function () {
       .attr('width', xHistScale(histData[0].x1) - xHistScale(histData[0].x0) - 1)
       .attr('fill', barColors[0])
       .attr('opacity', 0);
-
-    // cough title
-    g.append('text')
-      .attr('class', 'sub-title cough cough-title')
-      .attr('x', width / 2)
-      .attr('y', 60)
-      .text('cough')
-      .attr('opacity', 0);
-
-    // arrowhead from
-    // http://logogin.blogspot.com/2013/02/d3js-arrowhead-markers.html
-    svg.append('defs').append('marker')
-      .attr('id', 'arrowhead')
-      .attr('refY', 2)
-      .attr('markerWidth', 6)
-      .attr('markerHeight', 4)
-      .attr('orient', 'auto')
-      .append('path')
-      .attr('d', 'M 0,0 V 4 L6,2 Z');
-
-    g.append('path')
-      .attr('class', 'cough cough-arrow')
-      .attr('marker-end', 'url(#arrowhead)')
-      .attr('d', function () {
-        var line = 'M ' + ((width / 2) - 10) + ' ' + 80;
-        line += ' l 0 ' + 230;
-        return line;
-      })
-      .attr('opacity', 0);
   };
 
-  /**
-   * setupSections - each section is activated
-   * by a separate function. Here we associate
-   * these functions to the sections based on
-   * the section's index.
-   *
-   */
+  /*setupSections - each section is activated by a separate function. Here we associate these functions to the sections based on the section's index.*/
   var setupSections = function () {
     // activateFunctions are called each
     // time the active section changes
     activateFunctions[0] = showTitle;
     activateFunctions[1] = showFillerTitle;
-    activateFunctions[2] = showGrid;
-    activateFunctions[3] = highlightGrid;
-    activateFunctions[4] = showBar;
-    activateFunctions[5] = showHistPart;
+    activateFunctions[2] = showHistAll;
+    activateFunctions[3] = showFillerTitle;
+    activateFunctions[4] = showHistAll;
+    activateFunctions[5] = showFillerTitle;
     activateFunctions[6] = showHistAll;
-    activateFunctions[7] = showCough;
+    activateFunctions[7] = showFillerTitle;
     activateFunctions[8] = showHistAll;
+    activateFunctions[9] = showFillerTitle;
 
-    // updateFunctions are called while
-    // in a particular section to update
-    // the scroll progress in that section.
-    // Most sections do not need to be updated
-    // for all scrolling and so are set to
-    // no-op functions.
+
+    // updateFunctions are called while in a particular section to update the scroll progress in that section.
+    // Most sections do not need to be updated for all scrolling and so are set to no-op functions.
     for (var i = 0; i < 9; i++) {
       updateFunctions[i] = function () {};
     }
-    updateFunctions[7] = updateCough;
   };
 
-  /**
-   * ACTIVATE FUNCTIONS
-   *
-   * These will be called their
-   * section is scrolled to.
-   *
-   * General pattern is to ensure
-   * all content for the current section
-   * is transitioned in, while hiding
-   * the content for the previous section
-   * as well as the next section (as the
-   * user may be scrolling up or down).
-   *
-   */
-
-  /**
-   * showTitle - initial title
-   *
+  /* ACTIVATE FUNCTIONS
+   * These will be called their section is scrolled to.*/
+  /* showTitle - initial title
    * hides: count title
    * (no previous step to hide)
-   * shows: intro title
-   *
-   */
+   * shows: intro title*/
   function showTitle() {
     g.selectAll('.count-title')
       .transition()
@@ -352,130 +238,21 @@ var scrollVis = function () {
       .attr('opacity', 1.0);
   }
 
-  /**
-   * showFillerTitle - filler counts
-   *
+  /*showFillerTitle - filler counts
    * hides: intro title
-   * hides: square grid
-   * shows: filler count title
-   *
-   */
+   * hides: histogram
+   * shows: filler count title*/
   function showFillerTitle() {
     g.selectAll('.openvis-title')
       .transition()
       .duration(0)
       .attr('opacity', 0);
-
-    g.selectAll('.square')
-      .transition()
-      .duration(0)
-      .attr('opacity', 0);
-
-    g.selectAll('.count-title')
-      .transition()
-      .duration(600)
-      .attr('opacity', 1.0);
-  }
-
-  /**
-   * showGrid - square grid
-   *
-   * hides: filler count title
-   * hides: filler highlight in grid
-   * shows: square grid
-   *
-   */
-  function showGrid() {
-    g.selectAll('.count-title')
-      .transition()
-      .duration(0)
-      .attr('opacity', 0);
-
-    g.selectAll('.square')
-      .transition()
-      .duration(600)
-      .delay(function (d) {
-        return 5 * d.row;
-      })
-      .attr('opacity', 1.0)
-      .attr('fill', '#ddd');
-  }
-
-  /**
-   * highlightGrid - show fillers in grid
-   *
-   * hides: barchart, text and axis
-   * shows: square grid and highlighted
-   *  filler words. also ensures squares
-   *  are moved back to their place in the grid
-   */
-  function highlightGrid() {
-    hideAxis();
-    g.selectAll('.bar')
-      .transition()
-      .duration(600)
-      .attr('width', 0);
-
-    g.selectAll('.bar-text')
-      .transition()
-      .duration(0)
-      .attr('opacity', 0);
-
-
-    g.selectAll('.square')
-      .transition()
-      .duration(0)
-      .attr('opacity', 1.0)
-      .attr('fill', '#ddd');
-
-    // use named transition to ensure
-    // move happens even if other
-    // transitions are interrupted.
-    g.selectAll('.fill-square')
-      .transition('move-fills')
-      .duration(800)
-      .attr('x', function (d) {
-        return d.x;
-      })
-      .attr('y', function (d) {
-        return d.y;
-      });
-
-    g.selectAll('.fill-square')
-      .transition()
-      .duration(800)
-      .attr('opacity', 1.0)
-      .attr('fill', function (d) { return d.filler ? '#008080' : '#ddd'; });
-  }
-
-  /**
-   * showBar - barchart
-   *
-   * hides: square grid
-   * hides: histogram
-   * shows: barchart
-   *
-   */
-  function showBar() {
-    // ensure bar axis is set
-    showAxis(xAxisBar);
-
-    g.selectAll('.square')
-      .transition()
-      .duration(800)
-      .attr('opacity', 0);
-
-    g.selectAll('.fill-square')
-      .transition()
-      .duration(800)
-      .attr('x', 0)
-      .attr('y', function (d, i) {
-        return yBarScale(i % 3) + yBarScale.bandwidth() / 2;
-      })
-      .transition()
-      .duration(0)
-      .attr('opacity', 0);
-
+    
+//hides the axis
+    g.select('.x.axis')
+      .transition().duration(500)
+      .style('opacity', 0);
+//hides the histogram
     g.selectAll('.hist')
       .transition()
       .duration(600)
@@ -483,67 +260,20 @@ var scrollVis = function () {
       .attr('y', function () { return height; })
       .style('opacity', 0);
 
-    g.selectAll('.bar')
-      .transition()
-      .delay(function (d, i) { return 300 * (i + 1);})
-      .duration(600)
-      .attr('width', function (d) { return xBarScale(d.value); });
-
-    g.selectAll('.bar-text')
+    g.selectAll('.count-title')
       .transition()
       .duration(600)
-      .delay(1200)
       .attr('opacity', 1);
   }
 
-  /**
-   * showHistPart - shows the first part
-   *  of the histogram of filler words
-   *
-   * hides: barchart
-   * hides: last half of histogram
-   * shows: first half of histogram
-   *
-   */
-  function showHistPart() {
-    // switch the axis to histogram one
-    showAxis(xAxisHist);
 
-    g.selectAll('.bar-text')
-      .transition()
-      .duration(0)
-      .attr('opacity', 0);
-
-    g.selectAll('.bar')
-      .transition()
-      .duration(600)
-      .attr('width', 0);
-
-    // here we only show a bar if
-    // it is before the 15 minute mark
-    g.selectAll('.hist')
-      .transition()
-      .duration(600)
-      .attr('y', function (d) { return (d.x0 < 15) ? yHistScale(d.length) : height; })
-      .attr('height', function (d) { return (d.x0 < 15) ? height - yHistScale(d.length) : 0; })
-      .style('opacity', function (d) { return (d.x0 < 15) ? 1.0 : 1e-6; });
-  }
-
-  /**
-   * showHistAll - show all histogram
-   *
-   * hides: cough title and color
-   * (previous step is also part of the
-   *  histogram, so we don't have to hide
-   *  that)
-   * shows: all histogram bars
-   *
-   */
+/* showHistAll - show all of the histogram
+   * hides: cough title and color (previous step was also part of the histogram. But now it isn't so you need to hide the axis)*/
   function showHistAll() {
     // ensure the axis to histogram one
     showAxis(xAxisHist);
 
-    g.selectAll('.cough')
+    g.selectAll('.count-title')
       .transition()
       .duration(0)
       .attr('opacity', 0);
@@ -563,34 +293,7 @@ var scrollVis = function () {
       .style('opacity', 1.0);
   }
 
-  /**
-   * showCough
-   *
-   * hides: nothing
-   * (previous and next sections are histograms
-   *  so we don't have to hide much here)
-   * shows: histogram
-   *
-   */
-  function showCough() {
-    // ensure the axis to histogram one
-    showAxis(xAxisHist);
-
-    g.selectAll('.hist')
-      .transition()
-      .duration(600)
-      .attr('y', function (d) { return yHistScale(d.length); })
-      .attr('height', function (d) { return height - yHistScale(d.length); })
-      .style('opacity', 1.0);
-  }
-
-  /**
-   * showAxis - helper function to
-   * display particular xAxis
-   *
-   * @param axis - the axis to show
-   *  (xAxisHist or xAxisBar)
-   */
+  /*showAxis - helper function to display particular xAxis @param axis - the axis to show (xAxisHist or xAxisBar)*/
   function showAxis(axis) {
     g.select('.x.axis')
       .call(axis)
@@ -598,69 +301,19 @@ var scrollVis = function () {
       .style('opacity', 1);
   }
 
-  /**
-   * hideAxis - helper function
-   * to hide the axis
-   *
-   */
+  /*hideAxis - helper function to hide the axis*/
   function hideAxis() {
     g.select('.x.axis')
       .transition().duration(500)
       .style('opacity', 0);
   }
+  /* DATA FUNCTIONS: Used to coerce the data into the formats we need to visualize*/
 
-  /**
-   * UPDATE FUNCTIONS
-   *
-   * These will be called within a section
-   * as the user scrolls through it.
-   *
-   * We use an immediate transition to
-   * update visual elements based on
-   * how far the user has scrolled
-   *
-   */
-
-  /**
-   * updateCough - increase/decrease
-   * cough text and color
-   *
-   * @param progress - 0.0 - 1.0 -
-   *  how far user has scrolled in section
-   */
-  function updateCough(progress) {
-    g.selectAll('.cough')
-      .transition()
-      .duration(0)
-      .attr('opacity', progress);
-
-    g.selectAll('.hist')
-      .transition('cough')
-      .duration(0)
-      .style('fill', function (d) {
-        return (d.x0 >= 14) ? coughColorScale(progress) : '#008080';
-      });
-  }
-
-  /**
-   * DATA FUNCTIONS
-   *
-   * Used to coerce the data into the
-   * formats we need to visualize
-   *
-   */
-
-  /**
-   * getWords - maps raw data to
-   * array of data objects. There is
-   * one data object for each word in the speach
+  /*getWords - maps raw data to array of data objects. There is
+one data object for each word in the speach
    * data.
-   *
    * This function converts some attributes into
-   * numbers and adds attributes used in the visualization
-   *
-   * @param rawData - data read in from file
-   */
+   * numbers and adds attributes used in the visualization*/
   function getWords(rawData) {
     return rawData.map(function (d, i) {
       // is this word a filler word?
@@ -670,13 +323,6 @@ var scrollVis = function () {
       // time in minutes word was spoken
       d.min = Math.floor(d.time / 60);
 
-      // positioning for square visual
-      // stored here to make it easier
-      // to keep track of.
-      d.col = i % numPerRow;
-      d.x = d.col * (squareSize + squarePad);
-      d.row = Math.floor(i / numPerRow);
-      d.y = d.row * (squareSize + squarePad);
       return d;
     });
   }
@@ -745,7 +391,6 @@ var scrollVis = function () {
 
   /**
    * update
-   *
    * @param index
    * @param progress
    */
@@ -758,14 +403,11 @@ var scrollVis = function () {
 };
 
 
-/**
- * display - called once data
+
+ /* display - called once data
  * has been loaded.
  * sets up the scroller and
- * displays the visualization.
- *
- * @param data - loaded tsv data
- */
+ * displays the visualization.*/
 function display(data) {
   // create a new plot and
   // display it
